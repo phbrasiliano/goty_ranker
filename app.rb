da4f244c3390226b4e368e11dea970f8cd21e702
Phellipe <phbrasiliano@gmail.com>
2017-03-14 12:32:44 +0100

    adds basic interface

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ GAME_DIC = File.open("./files/games.marshal", "r"){ |file| Marshal.load(file)}
 final_list = []
 
 get '/' do
-  game1 = GAME_DIC[0]
-  game2 = GAME_DIC[1]
+  game1 = GAME_DIC[3]
+  game2 = GAME_DIC[25]
   erb(:index, :locals => {game1: game1, game2: game2})
 end
